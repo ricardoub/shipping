b11e60f89402e623a77d6798a8e6a5871f684bde
Ricardo Bomfim <ricardoub@gmail.com>
2017-05-15 20:10:40 -0300

    entrust: packege zizago/entrust instalation

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Zizaco\Entrust\EntrustServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -226,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Entrust' => Zizaco\Entrust\EntrustFacade::class,
     ],
 
 ];
