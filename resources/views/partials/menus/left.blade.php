266de30c4e42d9b9d051dcd928688fbf4cff1b40
Ricardo Bomfim <ricardoub@gmail.com>
2017-05-17 20:38:29 -0300

    menus - taks item added

--- resources/views/partials/menus/left.blade.php
+++ resources/views/partials/menus/left.blade.php
@@ -7,31 +7,35 @@
     </a>
   </li>
   @if (!auth::guest())
-    <li >
-      <a href="{{ route('tasks.index') }}">
-        <i class="fa fa-tasks fa-fw"></i>
-        Tarefas
-      </a>
-    </li>
+    @permission('task-menu')
+      <li >
+        <a href="{{ route('tasks.index') }}">
+          <i class="fa fa-tasks fa-fw"></i>
+          Tarefas
+        </a>
+      </li>
+    @endpermission
     <li class="dropdown">
       <a tabindex="0" data-toggle="dropdown" data-submenu="">
         <i class="fa fa-institution fa-fw"></i>
         Administração<span class="caret"></span>
       </a>
-      <ul class="dropdown-menu">
-        <li class="dropdown-submenu">
-          <a tabindex="0">Sistema</a>
-          <ul class="dropdown-menu">
-            <li class="">
-              <a href="{{ url('combos.index') }}" tabindex="0">
-                <i class="fa fa-list fa-fw"></i>
-                Caixas de seleção
-                <small class="text-muted">Role: #900</small>
-              </a>
-            </li>
-          </ul>
-        </li>
-      </ul>
+      @permission('combo-menu')
+        <ul class="dropdown-menu">
+          <li class="dropdown-submenu">
+            <a tabindex="0">Sistema</a>
+            <ul class="dropdown-menu">
+              <li class="">
+                <a href="{{ url('combos.index') }}" tabindex="0">
+                  <i class="fa fa-list fa-fw"></i>
+                  Caixas de seleção
+                  <small class="text-muted">Role: #900</small>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      @endpermission
     </li>
   @endif
 </ul>
