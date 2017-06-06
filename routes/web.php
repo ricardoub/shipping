36e75addc46d8a315054347f82c7ab4fa5cb363c
Ricardo Bomfim <ricardoub@gmail.com>
2017-06-05 21:14:03 -0300

    ticket: create route group

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,13 @@ Route::group(['prefix' => 'tasks', 'as' => 'tasks.'], function() {
   Route::get('delete/{id}',  ['as' => 'delete',  'uses' => 'TaskController@delete']);
   Route::post('destroy/{id}',['as' => 'destroy', 'uses' => 'TaskController@destroy']);
 });
+Route::group(['prefix' => 'tickets', 'as' => 'tickets.'], function() {
+  Route::get('',             ['as' => 'index',   'uses' => 'TicketController@index']);
+  Route::get('create',       ['as' => 'create',  'uses' => 'TicketController@create']);
+  Route::post('store',       ['as' => 'store',   'uses' => 'TicketController@store']);
+  Route::get('show/{id}',    ['as' => 'show',    'uses' => 'TicketController@show']);
+  Route::get('edit/{id}',    ['as' => 'edit',    'uses' => 'TicketController@edit']);
+  Route::post('update/{id}', ['as' => 'update',  'uses' => 'TicketController@update']);
+  Route::get('delete/{id}',  ['as' => 'delete',  'uses' => 'TicketController@delete']);
+  Route::post('destroy/{id}',['as' => 'destroy', 'uses' => 'TicketController@destroy']);
+});
